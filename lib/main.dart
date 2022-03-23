eaecb3cf9a18107acb5017b62592a5949879f6ac
Deonir <deonirluiz@yahoo.com.br>
2022-03-23 12:00:53 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:agenda_de_contatos/ui/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
